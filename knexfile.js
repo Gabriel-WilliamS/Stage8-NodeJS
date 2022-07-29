31facdd2084846f5ff5837d10fb9b1654d28b4df
gabriel-williams <gabrielwilliamcps@gmail.com>
2022-07-29 19:01:25 -0300

    ...

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,25 @@
 const path = require("path");
 
 module.exports = {
+  production: {
+    client: "sqlite3",
+    connection: {
+      filename: path.resolve(__dirname, "src", "database", "database.db")
+    },
+    pool: {
+      afterCreate: (conn, cb) => conn.run("PRAGMA foreign_keys = ON", cb)
+    },
+    migrations: {
+      directory: path.resolve(
+        __dirname,
+        "src",
+        "database",
+        "knex",
+        "migrations"
+      )
+    },
+    useNullAsDefault: true
+  },
   development: {
     client: "sqlite3",
     connection: {
